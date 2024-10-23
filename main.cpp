47e0bf28fefdb3f42fd5e11df35eb97df8618ebd
achamp3 <achamp20041111@gmail.com>
2024-10-22 21:08:50 -0400

    Modified and added comments

--- main.cpp
+++ main.cpp
@@ -8,22 +8,17 @@ using namespace std;
 int main()
 {
     bool isRunning = true;
-    int score = 0;
-    //Make player
-    Player *aPlayer = new Player();
-    //Make enemy
-    Enemy *aEnemy = new Enemy();
-    //Round 1. FIGHT.
+    int score = 0; //Initialize score to 0.
+    Player *aPlayer = new Player(); //Make player
+    Enemy *aEnemy = new Enemy(); //Make enemy
     cout << "ROUND 1. FIGHT. (Enter 5 to end game)" << endl;
+
     while(isRunning)
     {
-        //Print the player's choices for moves.
-        //Do the player's move on the enemy.
-        aPlayer->doMove(aEnemy);
-        //Print enemy's HP.
-        cout << "Enemy's HP: "<< aEnemy->getEnemyHealth() << endl;
-        //Check if the enemy is dead. hp = 0. respawn enemy. Track high score.
-        if(aEnemy->getEnemyHealth() == 0)
+        aPlayer->doMove(aEnemy); //Print the player's choices for moves. Do the player's move on the enemy.
+        cout << "Enemy's HP: "<< aEnemy->getEnemyHealth() << endl; //Print enemy's HP.
+        
+        if(aEnemy->getEnemyHealth() == 0) //Check if the enemy is dead. hp = 0. respawn enemy. Track high score.
         {
             delete aEnemy;
             aEnemy = new Enemy();
@@ -31,12 +26,11 @@ int main()
             score++;
             cout << "Score: " << score << endl;
         }
-        //Do enemy's move on the player.
-        aEnemy->doMove(aPlayer);
-        //Print player's HP.
-        cout << "Your HP: "<< aPlayer->getPlayerHealth() << endl;
-        //Check if the player is dead. hp = 0. end game. print you died.
-        if(aPlayer->getPlayerHealth() == 0)
+
+        aEnemy->doMove(aPlayer); //Do enemy's move on the player.
+        cout << "Your HP: "<< aPlayer->getPlayerHealth() << endl; //Print player's HP.
+        
+        if(aPlayer->getPlayerHealth() == 0) //Check if the player is dead (hp = 0), if true, then end game. Print "You died." Print player score.
         {
             cout << "You died. Try again next time." << endl;
             cout << "Score: " << score << endl;
